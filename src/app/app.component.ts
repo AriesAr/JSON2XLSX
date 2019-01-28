263f54ed6f917b722b93af8c2dc183f97029af96
Adithya Rayaprolu <43945063+AriesAr@users.noreply.github.com>
2019-01-28 20:25:10 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as alas from '../../bower_components/alasql/dist/alasql';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'J2Xls';
+}
